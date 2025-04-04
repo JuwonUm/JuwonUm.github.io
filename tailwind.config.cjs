8c58f963b70d732066c78d1ac402bf19acfea549
u-juwon <ujw0620@naver.com>
2025-04-05 01:07:38 +0900

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,26 @@ module.exports = {
 		require("daisyui")
 	],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: [
+			"light",
+			"dark",
+			"pastel",
+			{
+			  pastelDark: {
+				"primary": "#d1c1d7",
+				"secondary": "#e0b2a4",
+				"accent": "#c4c3f7",
+				"neutral": "#2b3440",
+				"base-100": "#1E1E24",   
+				"base-200": "#1A1A20",   
+				"base-content": "#ffffff", 
+				"info": "#86d3f4",
+				"success": "#b4f7c1",
+				"warning": "#f6d86b",
+				"error": "#fc9090",
+			  },
+			},
+		  ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
